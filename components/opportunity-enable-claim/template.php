7927638166acc2b3826c0d187bda743ef493e39a
erleibiazzio <erleibiazzio@gmail.com>
2023-08-14 18:06:54 -0300

    Ajusta para exibir os campos data inicial data final e hora final do período de recuros de forma separada

--- components/opportunity-enable-claim/template.php
+++ components/opportunity-enable-claim/template.php
@@ -18,6 +18,12 @@ $this->import('
         <input type="checkbox" id="resource" v-model="isActiveClaim" @click="autoSave()" />
         <label for="resource"><?= i::__("Habilitar Recurso") ?></label>
     </div>
+    <div class="grid-12">
+        <entity-field :entity="entity" classes="col-3 sm:col-12" prop="claimFrom"></entity-field>
+        <entity-field :entity="entity" classes="col-3 sm:col-12" prop="claimTo"></entity-field>
+        <entity-field :entity="entity" classes="col-2 sm:col-12" prop="claimTimeTo"></entity-field>
+    </div>
+
     <div v-if="isActiveClaim" class="opportunity-enable-claim__email">
         <label class="opportunity-enable-claim__label" for="input">
             <h5 class="semibold opportunity-enable-claim__subtitle"><?= i::__("Insira o email que receberá as solicitações") ?></h5>
