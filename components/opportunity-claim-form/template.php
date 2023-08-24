e9fbf62a69c49c4798880344f28179d5df286555
erleibiazzio <erleibiazzio@gmail.com>
2023-08-24 09:21:39 -0300

    Ajusta implementação da tela de solicitação de recurso levando em conta a visão de administrador e proponente

--- components/opportunity-claim-form/template.php
+++ components/opportunity-claim-form/template.php
@@ -8,27 +8,26 @@
 use MapasCulturais\i;
 
 $this->import('
+    entity-files-list
     mc-container
     mc-modal
     mc-icon
 ');
 ?>
-<div v-if="isActive()">
+<div v-if="isActive() && !isAdmin">
     <mc-modal :title="modalTitle" classes="opportunity-claim-form" button-classes="opportunity-claim-form__buttonlabel">
         <template #default>
-            <div>
-                <h4><?php i::_e('Envie um modelo de documento que deve ser anexado pelos solicitantes') ?></h4>
-                <div>
-                    <input type="file" name="formClaimUploadSample" @change="setFile()" ref="file">
-                </div>
-                <div v-if="filesUpload.url">
-                    <ul>
-                        <li>
-                            <a :href="filesUpload.url" target="_blank" download >{{filesUpload.name}}</a>
-                        </li>
-                    </ul>
-                </div>
+            <div v-if="filesUpload">
+                <entity-files-list :entity="entity" group="formClaimUpload" title="<?php i::_e('Anexar documento') ?>" :editable="canManipulate"></entity-files-list>
             </div>
+            <div v-if="filesSample.url">
+                <ul>
+                    <li>
+                        <a :href="filesSample.url" target="_blank" download><?php i::_e('Baixe o arquivo modelo para o anexo') ?> <mc-icon name="download"></mc-icon></a>
+                    </li>
+                </ul>
+            </div>
+
             <div class="opportunity-claim-form__content">
                 <h5 class="semibold opportunity-claim-form__label"><?php i::_e('Descreva abaixo os motivos do recurso') ?></h5>
                 <textarea v-model="claim.message" id="message" class="opportunity-claim-form__textarea"></textarea>
@@ -41,16 +40,19 @@ $this->import('
         <template #button="modal">
             <h5 class="opportunity-claim-form__resource bold"><?php i::_e('Discorda do resultado?') ?></h5>
             <div>
-                <?php i::_e('Período do recurso de recurso');?> {{claimFromDate}} a {{claimclaimTo}} 
+                <?php i::_e('Período do recurso de recurso'); ?> {{claimFromDate}} a {{claimclaimTo}}
             </div>
             <button class="button button--primary-outline" @click="open(modal)"><?php i::_e('Solicitar Recurso') ?></button>
         </template>
     </mc-modal>
-    <div v-if="filesSample.url">
-        <ul>
-            <li>
-                <a :href="filesSample.url" target="_blank" download ><?php i::_e('Baixar arquivo de exemplo') ?></a>
-            </li>
-        </ul>
+</div>
+
+<div v-if="filesUpload && isActive() && isAdmin">
+    <div>
+        <entity-files-list :entity="entity" group="formClaimUpload" title="<?php i::_e('Arquivo de recurso anexado') ?>"></entity-files-list>
+    </div>
+    <div>
+        <button class="button button--primary" v-if="!entity.acceptClaim" @click="acceptClaim()"><?php i::_e('Aceitar arquivo') ?></button>
+        <button class="button button--primary" @click="refuseClaim()"><?php i::_e('Recusar arquivo') ?></button>
     </div>
 </div>
